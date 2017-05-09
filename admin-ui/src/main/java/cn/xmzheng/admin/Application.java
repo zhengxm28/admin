a89281587073532e7b5c92a3241304f6824e1350
xmpure <afuhao147@126.com>
2017-05-09 09:42:46 +0800

    add module dependency

--- admin-ui/src/main/java/cn/xmzheng/admin/Application.java
+++ admin-ui/src/main/java/cn/xmzheng/admin/Application.java
@@ -1,5 +1,13 @@
 package cn.xmzheng.admin;
 
-public class Application {
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
+@SpringBootApplication
+@EnableCaching
+public class Application {
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
 }
