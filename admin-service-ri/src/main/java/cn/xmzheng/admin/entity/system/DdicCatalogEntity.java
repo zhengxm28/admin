015d694808a5643d58286a1db97f75ad17d83ba3
xmpure <afuhao147@126.com>
2017-05-09 15:31:33 +0800

    添加系统类的entity

---
+++ admin-service-ri/src/main/java/cn/xmzheng/admin/entity/system/DdicCatalogEntity.java
@@ -0,0 +1,8 @@
+package cn.xmzheng.admin.entity.system;
+
+import javax.persistence.Entity;
+
+@Entity(name = "ddic_catalog")
+public class DdicCatalogEntity {
+
+}
---
+++ admin-service-ri/src/main/java/cn/xmzheng/admin/entity/system/DdicItemEntity.java
@@ -0,0 +1,8 @@
+package cn.xmzheng.admin.entity.system;
+
+import javax.persistence.Entity;
+
+@Entity(name = "ddic_item")
+public class DdicItemEntity {
+
+}
