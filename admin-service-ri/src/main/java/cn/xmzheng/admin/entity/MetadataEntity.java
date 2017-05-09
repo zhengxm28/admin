b8a3f8eaf98f778908ffec580d18734b53f712d9
zhengfc <zhengfc323@gmail.com>
2017-05-10 02:00:55 +0800

    add system init repository

--- admin-service-ri/src/main/java/cn/xmzheng/admin/entity/MetadataEntity.java
+++ admin-service-ri/src/main/java/cn/xmzheng/admin/entity/MetadataEntity.java
@@ -1,18 +1,54 @@
 package cn.xmzheng.admin.entity;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 
 @Entity(name = "metadata")
 public class MetadataEntity {
-	private String id;
+	private String module;  //模块
+	private String cnName;  //中文名称
+	private String tableName;  //对应表名
+	private int status;  //状态标识位
+	private int sortNo;  //排序号
 
 	@Id
-	public String getId() {
-		return id;
+	public String getModule() {
+		return module;
 	}
-
-	public void setId(String id) {
-		this.id = id;
+	public void setModule(String module) {
+		this.module = module;
+	}
+	
+	@Column
+	public String getCnName() {
+		return cnName;
+	}
+	public void setCnName(String cnName) {
+		this.cnName = cnName;
+	}
+	
+	@Column
+	public String getTableName() {
+		return tableName;
+	}
+	public void setTableName(String tableName) {
+		this.tableName = tableName;
+	}
+	
+	@Column
+	public int getStatus() {
+		return status;
+	}
+	public void setStatus(int status) {
+		this.status = status;
+	}
+	
+	@Column
+	public int getSortNo() {
+		return sortNo;
+	}
+	public void setSortNo(int sortNo) {
+		this.sortNo = sortNo;
 	}
 }
--- admin-service-ri/src/main/java/cn/xmzheng/admin/entity/system/EventLogEntity.java
+++ admin-service-ri/src/main/java/cn/xmzheng/admin/entity/system/EventLogEntity.java
@@ -1,8 +1,20 @@
 package cn.xmzheng.admin.entity.system;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
 
 @Entity(name = "event_log")
 public class EventLogEntity {
+	private int id;
 
+	@Id
+	@GeneratedValue
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
 }
