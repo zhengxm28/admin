46e5b239d03e7a36b9eb47e5d7e08011de66c82e
xmpure <afuhao147@126.com>
2017-05-15 11:15:19 +0800

    add jdbcTemplate

---
+++ admin-service-ri/src/main/java/cn/xmzheng/admin/repository/custom/JdbcCustom.java
@@ -0,0 +1,5 @@
+package cn.xmzheng.admin.repository.custom;
+
+public interface JdbcCustom {
+
+}
---
+++ admin-service-ri/src/main/java/cn/xmzheng/admin/repository/custom/impl/JdbcCustomImpl.java
@@ -0,0 +1,7 @@
+package cn.xmzheng.admin.repository.custom.impl;
+
+import cn.xmzheng.admin.repository.custom.JdbcCustom;
+
+public class JdbcCustomImpl implements JdbcCustom {
+
+}
