46531c7c823d3a9d39b9ddaaa0d0e34f38f169ed
xmpure <afuhao147@126.com>
2017-05-15 13:21:02 +0800

    add static sample #spring-boot-sample-web-static

--- admin-service-ri/src/main/java/cn/xmzheng/admin/Application.java
+++ admin-service-ri/src/main/java/cn/xmzheng/admin/Application.java
@@ -2,10 +2,8 @@ package cn.xmzheng.admin;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
-@EnableCaching
 public class Application {
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
--- admin-ui/src/main/java/cn/xmzheng/admin/Application.java
+++ admin-ui/src/main/java/cn/xmzheng/admin/Application.java
@@ -2,11 +2,17 @@ package cn.xmzheng.admin;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-@EnableCaching
-public class Application {
+public class Application extends SpringBootServletInitializer {
+	
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(Application.class);
+	}
+
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
