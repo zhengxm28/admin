1c974a859aa216c261dc9609aee53aa187c3f3c0
xmpure <afuhao147@126.com>
2017-05-26 09:57:57 +0800

    fill log entity

--- admin-service-ri/src/main/java/cn/xmzheng/admin/entity/system/SystemLogEntity.java
+++ admin-service-ri/src/main/java/cn/xmzheng/admin/entity/system/SystemLogEntity.java
@@ -1,12 +1,22 @@
 package cn.xmzheng.admin.entity.system;
 
+import java.sql.Date;
+
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 
-@Entity(name = "event_log")
+@Entity(name = "system_log")
 public class SystemLogEntity {
 	private int id;
+	private String tableName;
+	private String action;
+	private String preData;
+	private String postData;
+	private String operator;
+	private Date operateTime;
+	private String memo;
 
 	@Id
 	@GeneratedValue
@@ -17,4 +27,68 @@ public class SystemLogEntity {
 	public void setId(int id) {
 		this.id = id;
 	}
+
+	@Column
+	public String getTableName() {
+		return tableName;
+	}
+
+	@Column
+	public String getAction() {
+		return action;
+	}
+
+	@Column
+	public String getPreData() {
+		return preData;
+	}
+
+	@Column
+	public String getPostData() {
+		return postData;
+	}
+
+	@Column
+	public String getOperator() {
+		return operator;
+	}
+
+	@Column
+	public Date getOperateTime() {
+		return operateTime;
+	}
+
+	@Column
+	public String getMemo() {
+		return memo;
+	}
+
+	public void setTableName(String tableName) {
+		this.tableName = tableName;
+	}
+
+	public void setAction(String action) {
+		this.action = action;
+	}
+
+	public void setPreData(String preData) {
+		this.preData = preData;
+	}
+
+	public void setPostData(String postData) {
+		this.postData = postData;
+	}
+
+	public void setOperator(String operator) {
+		this.operator = operator;
+	}
+
+	public void setOperateTime(Date operateTime) {
+		this.operateTime = operateTime;
+	}
+
+	public void setMemo(String memo) {
+		this.memo = memo;
+	}
+	
 }
