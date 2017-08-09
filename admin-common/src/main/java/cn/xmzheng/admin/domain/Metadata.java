1828501b57776628a827630d72f74b58fd6c91dd
xmpure <afuhao147@126.com>
2017-08-09 13:41:55 +0800

    add metadata domain

---
+++ admin-common/src/main/java/cn/xmzheng/admin/domain/Metadata.java
@@ -0,0 +1,20 @@
+package cn.xmzheng.admin.domain;
+
+import java.beans.PropertyDescriptor;
+
+import org.apache.commons.collections.MultiMap;
+import org.apache.commons.collections.map.MultiValueMap;
+
+import lombok.Data;
+
+public @Data class Metadata {
+	private String module; // 模块
+	private String cnName; // 中文名称
+	private String tableName; // 对应表名
+	private int status; // 状态标识位
+	private int sortNo; // 排序号
+	private Class<?> entityJavaType; // 数据层class
+	private PropertyDescriptor[] pds; // 数据层pd描述
+	private Object repositoryBean; // repository类对象
+	private MultiMap ddicInfo = new MultiValueMap(); // 数据字典信息
+}
--- admin-service-ri/src/main/java/cn/xmzheng/admin/entity/MetadataEntity.java
+++ admin-service-ri/src/main/java/cn/xmzheng/admin/entity/MetadataEntity.java
@@ -6,48 +6,53 @@ import javax.persistence.Id;
 
 @Entity(name = "metadata")
 public class MetadataEntity {
-	private String module;  //模块
-	private String cnName;  //中文名称
-	private String tableName;  //对应表名
-	private int status;  //状态标识位
-	private int sortNo;  //排序号
+	private String module; // 模块
+	private String cnName; // 中文名称
+	private String tableName; // 对应表名
+	private int onOff; // 开关标识位
+	private int sortNo; // 排序号
 
 	@Id
 	public String getModule() {
 		return module;
 	}
+
 	public void setModule(String module) {
 		this.module = module;
 	}
-	
+
 	@Column
 	public String getCnName() {
 		return cnName;
 	}
+
 	public void setCnName(String cnName) {
 		this.cnName = cnName;
 	}
-	
+
 	@Column
 	public String getTableName() {
 		return tableName;
 	}
+
 	public void setTableName(String tableName) {
 		this.tableName = tableName;
 	}
-	
-	@Column
-	public int getStatus() {
-		return status;
+
+	@Column(name = "switch")
+	public int getOnOff() {
+		return onOff;
 	}
-	public void setStatus(int status) {
-		this.status = status;
+
+	public void setOnOff(int onOff) {
+		this.onOff = onOff;
 	}
-	
+
 	@Column
 	public int getSortNo() {
 		return sortNo;
 	}
+
 	public void setSortNo(int sortNo) {
 		this.sortNo = sortNo;
 	}
