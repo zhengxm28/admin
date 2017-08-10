794747fe812119c830cd3c7674fcebe86185114d
fczheng <fczheng@99wuxian.com>
2017-08-10 13:19:13 +0800

    refactor general controller

--- admin-controller/src/main/java/cn/xmzheng/admin/controller/GeneralController.java
+++ admin-controller/src/main/java/cn/xmzheng/admin/controller/GeneralController.java
@@ -3,7 +3,6 @@ package cn.xmzheng.admin.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -17,16 +16,14 @@ public class GeneralController {
 	IService service;
 
 	@PostMapping("/{module}/{mode}")
-	ResponseEntity<?> general(@PathVariable String module, @PathVariable String mode, DtoModel inModel) {
+	ResponseEntity<?> general(DtoModel inModel) {
 		inModel.setAccess(false);
 		DtoModel result = service.excute(inModel);
 		return DtoConvert.convertResponse(result);
 	}
 
-	@GetMapping("/{module}/list/{page}")
-	ResponseEntity<?> generalAccess(@PathVariable String module, @PathVariable int page,
-			DtoModel inModel) {
-		inModel.setCurrentPage(page);
+	@GetMapping("/{module}/list/{currentPage}")
+	ResponseEntity<?> generalAccess(DtoModel inModel) {
 		inModel.setAccess(true);
 		DtoModel result = service.excute(inModel);
 		return DtoConvert.convertResponse(result);
