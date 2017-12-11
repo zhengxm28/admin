564d1d76edf55d15c464895910fa09e8af215815
xmpure <afuhao147@126.com>
2017-12-11 15:20:06 +0800

    enable redis cache

--- admin-system/src/main/java/cn/xmzheng/admin/system/Application.java
+++ admin-system/src/main/java/cn/xmzheng/admin/system/Application.java
@@ -7,6 +7,9 @@ import javax.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.Cache;
+import org.springframework.cache.CacheManager;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -15,8 +18,10 @@ import org.springframework.web.bind.annotation.RestController;
 @SpringBootApplication
 @RestController
 @EnableRedisHttpSession(maxInactiveIntervalInSeconds=30*60*1000)
+@EnableCaching
 public class Application {
-	@Autowired StringRedisTemplate template;;
+	@Autowired StringRedisTemplate template;
+	@Autowired CacheManager cacheManager;
 	
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
@@ -24,6 +29,8 @@ public class Application {
 	
 	@GetMapping("/test")
 	public String test(HttpSession session) {
+		Cache cache = cacheManager.getCache("spring:session:sessions");
+		cache.put(session.getId(), "test");
 		Set<Object> keys = template.opsForHash().keys("spring:session:sessions:" + session.getId());
 		System.out.println(keys);
 		return session.getId();
