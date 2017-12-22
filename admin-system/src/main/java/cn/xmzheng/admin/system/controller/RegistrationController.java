1e85598bb67e40e4ec717a258b805f77333edeaa
xmpure <afuhao147@126.com>
2017-12-22 16:34:44 +0800

    测试redis

--- admin-system/src/main/java/cn/xmzheng/admin/system/controller/RegistrationController.java
+++ admin-system/src/main/java/cn/xmzheng/admin/system/controller/RegistrationController.java
@@ -4,6 +4,7 @@ import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cache.CacheManager;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -15,6 +16,8 @@ public class RegistrationController {
 	@Autowired EurekaClient eurekaClient;
 	@Autowired
 	CacheManager cacheManager;
+	@Autowired
+	StringRedisTemplate template;
 	/**
 	 * @return
 	 */
@@ -25,7 +28,16 @@ public class RegistrationController {
 	
 	@GetMapping("/test")
 	public String test(HttpSession session) {
-		cacheManager.getCache("admin-name").put(session.getId(), "test");
-		return session.getId();
+		cacheManager.getCache("admin:system").put(session.getId(), "test");
+		return "test";
+	}
+	
+	@GetMapping("/testRedis")
+	public String testRedis() {
+		for (int i=0; i<50000; i++) {
+			System.out.println(template.opsForHash().entries("aaaa"));
+			System.out.println(i);
+		}
+		return "okay";
 	}
 }
