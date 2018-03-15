9289b6ae619cd78a79b34ccfcd33e3bd9cac12ed
xmpure <afuhao147@126.com>
2018-03-15 18:15:57 +0800

    MetaService接口定义

--- admin-common/src/main/java/cn/xmzheng/admin/api/service/MetadataService.java
+++ admin-common/src/main/java/cn/xmzheng/admin/api/service/MetadataService.java
@@ -1,5 +1,13 @@
 package cn.xmzheng.admin.api.service;
 
-public interface MetadataService {
+import java.util.List;
 
+import cn.xmzheng.admin.domain.Metadata;
+
+public interface MetadataService {
+	List<Metadata> findAll();
+	Metadata findOne(String module);
+	
+	List<Metadata> findGroup(String group);
+	Metadata findOne(Class<?> entityClass);
 }
